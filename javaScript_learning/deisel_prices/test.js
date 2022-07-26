9e4391652afe9b08cbd328dad79eec0eddefd119
Aditya Kumar <aditya.kumar@indianexpress.com>
2022-07-26 14:08:32 +0530

    function working

--- javaScript_learning/deisel_prices/test.js
+++
@@ -1,1 +0,0 @@
-let priceUpdate = document.querySelector()
---
+++ javaScript_learning/deisel_prices/test1.js
@@ -0,0 +1,6 @@
+let priceUpdate = document.querySelectorAll(".table-data")[0];
+
+priceUpdate.addEventListener("click", function() {
+    console.log("clicked!");
+});
+
