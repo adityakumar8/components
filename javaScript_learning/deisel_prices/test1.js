6d00df1f0359aa67f70f415eb110486f7b7a04b3
Aditya Kumar <aditya.kumar@indianexpress.com>
2022-07-26 14:47:40 +0530

    promises added partially

--- javaScript_learning/deisel_prices/test1.js
+++ javaScript_learning/deisel_prices/test1.js
@@ -1,11 +1,25 @@
-let priceUpdate = document.querySelectorAll(".table-data")[0];
+let priceUpdate = document.querySelectorAll(".table-data")[3];
 
+let serverURL = "https://jsonplaceholder.typicode.com/users";
+
+function getPriceData(input) {
+    return serverURL + "?" + "text" + input
+}
+
+
+function clickHandler() {
+    fetch(getPriceData(data))
+    .then(response => response.json())
+    .then(json => {
+
+    })
+}
 // priceUpdate.addEventListener("click", function eventHandler() {
 //     console.log("clicked!");
 // });
 
 
-function clickEventHandler() {
-    console.log("clicked!");
-}
-priceUpdate.addEventListener("click",clickEventHandler)
+// function clickEventHandler() {
+//     console.log("clicked!");
+// }
+// priceUpdate.addEventListener("click",clickEventHandler)
